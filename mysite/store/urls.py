d5e106d884e79ae1b1cbc13d58f03a510f199d9f
Animkim <set-vlad@yandex.ru>
2016-05-04 06:31:09 +0700

    !

--- mysite/store/urls.py
+++ mysite/store/urls.py
@@ -3,7 +3,9 @@ from store import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index),
-    url(r'^(?P<category_name>[\w\-]+)/$', views.category)
-    #url(r'^[\w\-]+/(?P<category_name>[\w\-]+)/$', views.category)
-    #url(r'^[\w\-]/[\w\-]/(?P<category_name>[\w\-])/$', views.category)
+    url(r'^(?P<category_name>[\w\-]+)/$', views.category),
+    url(r'^[\w\-]+/(?P<category_name>[\w\-]+)/$', views.category),
+    url(r'^[\w\-]+/[\w\-]+/(?P<category_name>[\w\-]+)/$', views.category),
+    url(r'^/search/$', views.search),
+
 )
--- mysite/store/views.py
+++ mysite/store/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render_to_response
 from mysite.store.models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
-    item = Product.objects.all()
     category = Category.objects.filter(parent=None)
+    item = Product.objects.all()
+    paginator = Paginator(item, 12)
+    page = request.GET.get('page', 1)
+    try:
+        item = paginator.page(page)
+    except PageNotAnInteger:
+        item = paginator.page(1)
+    except EmptyPage:
+        item = paginator.page(paginator.num_pages)
+
     return render_to_response('index_1.html', {'items': item, 'categores': category})
 
 
@@ -14,3 +24,8 @@ def category(request, category_name):
     cat = Category.objects.get(slug=category_name)
     category_parent = cat.children.all()
     return render_to_response('category_1.html', {'items': item, 'categores': category_parent})
+
+
+def search(request):
+    pass
+
