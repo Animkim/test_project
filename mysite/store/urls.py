ed045dda1058f122ab609b210c714dbef89b55d6
Animkim <set-vlad@yandex.ru>
2016-05-15 14:49:20 +0700

    Конфликт

--- mysite/store/urls.py
+++ mysite/store/urls.py
@@ -7,9 +7,5 @@ urlpatterns = patterns('',
     url(r'^search/$', views.search),
     url(r'([\D\-]+)/$', views.category),
     url(r'[\w\-]+/(?P<slug>[\w\-]+)/$', views.product_page),
-<<<<<<< HEAD
-=======
-
->>>>>>> 8c1882331db924f0e42170ba518de5ee70c1708b
-)
+    )
 
