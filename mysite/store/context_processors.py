659885980b752aa423b84547c011c7a2827c6281
Vlad Svetlicnyi <aminkims@gmail.com>
2016-05-18 16:31:17 +0700

    Дерево категорий одним запросом

--- mysite/store/context_processors.py
+++ mysite/store/context_processors.py
@@ -4,13 +4,14 @@ from models import Category
 
 def category_list(request):
     categories = list(Category.objects.all())
-    def rel(all_cat, cat_par):
-        a = {}
-        for cat in all_cat:
-            if cat_par.id == cat.parent_id:
-                a.update({cat: rel(all_cat, cat)})
-        return a
 
-    cat = {c: rel(categories, c) for c in categories if not c.parent_id}
-    return {"categories": cat}
+    def search_child(all_categories, parent):
+        all_child = {}
+        for category in all_categories:
+            if parent.id == category.parent_id:
+                all_child.update({category: search_child(all_categories, category)})
+        return all_child
+
+    category_tree = {category: search_child(categories, category) for category in categories if not category.parent_id}
+    return {"categories": category_tree}
 
